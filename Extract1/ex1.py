9ac9f795f9ba88c5295f61b4b56b2e52af7e01d8
Roshan Shrestha <roshan_pra@hotmail.com>
2020-06-09 23:01:31 +0545

    added program for ppt

---
+++ Extract1/ex1.py
@@ -0,0 +1,34 @@
+# Import necessary libraries
+import requests
+import re
+from bs4 import BeautifulSoup
+import urllib.request
+
+#Requesting response from the webpage
+html = requests.get('http://www.knockhardy.org.uk/ppoints.htm')
+
+#Converting the webpage into beautiful soup object
+soup = BeautifulSoup(html.content,'lxml')
+
+#Creating an empty list to store links
+
+links = []
+
+#Finding and adding links in the list
+for link in soup.find_all('a', attrs = {'href': re.compile('.ppt$')}):
+    links.append(link.get('href'))
+   
+#Since there are many links of the pdf files, I will loop over only first 10 links
+
+links = sorted(links, key = lambda a: a.lower().split('/')[-1])
+
+
+
+
+for i in range(10):
+    urllib.request.urlretrieve('http://www.knockhardy.org.uk/'+links[i], '/home/roshan/Desktop/files/'+links[i].split('/')[-1])
+    
+
+    
+    
+    
